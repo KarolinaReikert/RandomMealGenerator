0be071dc4f98b53b35c0f1130c8de7b749e8cc4a
KarolinaReikert <karolinar12@gmil.com>
2023-05-14 21:25:15 +0300

    Combining multiple people's codes and created one project.

--- Meal.cs
+++ Meal.cs
@@ -6,12 +6,60 @@ using System.Threading.Tasks;
 
 namespace RandomMealGenerator
 {
-    public class Meal
+    // Example 1
+    //public class Meal
+    //{
+    //    public string Name { get; set; }
+    //    public int Price { get; set; }
+    //    public string[] Recipe { get; set; }
+    //    public string Country { get; set; }
+    //    public int Id { get; set; }
+    //}
+
+    // Example 2
+    public class Rootobject
     {
-        public string Name { get; set; }
-        public int Price { get; set; }
-        public string[] Recipe { get; set; }
-        public string Country { get; set; }
-        public int Id { get; set; }
+
+        public string q { get; set; }
+        public int from { get; set; }
+        public int to { get; set; }
+        public bool more { get; set; }
+        public int count { get; set; }
+        public Hit[] hits { get; set; }
+
+    }
+
+    public class Hit
+    {
+        public Recipe recipe { get; set; }
+    }
+
+
+
+    public class Recipe
+    {
+        public string label { get; set; }
+        public string source { get; set; }
+        public string url { get; set; }
+        public string[] ingredientLines { get; set; }
+        public Ingredient[] ingredients { get; set; }
+        public float calories { get; set; }
+        public float totalWeight { get; set; }
+        public float totalTime { get; set; }
+        public string[] cuisineType { get; set; }
+        public string[] mealType { get; set; }
+        public string[] dishType { get; set; }
+
+
+        public class Ingredient
+        {
+            public string text { get; set; }
+            public float quantity { get; set; }
+            public string measure { get; set; }
+            public string food { get; set; }
+            public float weight { get; set; }
+            public string foodCategory { get; set; }
+            public string foodId { get; set; }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,121 +3,89 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.IO;
+using Newtonsoft.Json;
+using System.Diagnostics.Metrics;
 
 namespace RandomMealGenerator
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //string json = @"
-            //[
-            //    {
-            //        ""Name"": ""Omlette"",
-            //        ""Price"": 4,
-            //        ""Recipe"": [""eggs"", ""vegetables""],
-            //        ""Country"": ""Italy""
-            //    },
-            //    {
-            //        ""Name"": ""Cepelinai"",
-            //        ""Price"": 7,
-            //        ""Recipe"": [""meat"", ""potatoes"", ""milk"", ""salt""],
-            //        ""Country"": ""Spain""
-            //    },
-            //    {
-            //        ""Name"": ""Pizza"",
-            //        ""Price"": 10,
-            //        ""Recipe"": [""flour"", ""cheese"", ""tomatoes"", ""peper"", ""water""],
-            //        ""Country"": ""Italy""
-            //    },
-            //    {
-            //        ""Name"": ""Potatoe pancakes"",
-            //        ""Price"": 3,
-            //        ""Recipe"": [""potatoes"", ""eggs"", ""flour"", ""butter""],
-            //        ""Country"": ""Lithuania""
-            //    },
-            //    {
-            //        ""Name"": ""Soup"",
-            //        ""Price"": 2,
-            //        ""Recipe"": [""water"", ""vegetables"", ""meat""],
-            //        ""Country"": ""Latvia""
-            //    }
-            //]";
-            string jsonFilePath = "C:\\Users\\Admin\\Desktop\\.NET kursas\\VsStudioProjects\\RandomMealGenerator\\mealsData.json";
-            string json = File.ReadAllText(jsonFilePath);
-            Meal[] meals = JsonSerializer.Deserialize<Meal[]>(json);
 
-            GetRandomRecepe(meals);
+            // Example 1
+            //string jsonFilePath = "C:\\Users\\Admin\\Desktop\\.NET kursas\\VsStudioProjects\\RandomMealGenerator\\mealsData.json";
+            //string json = File.ReadAllText(jsonFilePath);
+            //Meal[] meals = JsonSerializer.Deserialize<Meal[]>(json);
+            //GetRandomRecepe(meals);
 
+            // Example 2
+            Console.WriteLine("Choose a country: ");
+            string country = Console.ReadLine();
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync($"https://api.edamam.com/search?app_id=38c4fcc6&app_key=024de75c4492cec0c489343537b5e7ca&from=0&to=10&q={country}").Result;
+
+            Rootobject ApiResultObject = JsonConvert.DeserializeObject<Rootobject>(response);
+            GetRandomRecepe(ApiResultObject, country);
         }
 
-        static void GetRandomRecepe(Meal[] meals)
+        // Example 2
+        public static void GetRandomRecepe(Rootobject ApiResultObject, string country)
         {
-            if (meals != null)
+            if (ApiResultObject.hits.Length > 0)
             {
-                Console.Write("Enter country: ");
-                string input = Console.ReadLine();
+                // We are taking random from 0 to count.
+                var recipe = ApiResultObject.hits.ElementAt(new Random().Next(0, ApiResultObject.hits.Count())).recipe;
 
-                IEnumerable<Meal> selectedMeals = meals.Where(m => m.Country.ToLower() == input.ToLower());
-
-               Console.WriteLine("------>" + selectedMeals.Count());
-
-
-               Console.WriteLine("------>" + selectedMeals.First().Name);
-               Meal[] test =  selectedMeals.ToArray();
-               
-
-                Random random = new Random();
-                int randomIndex = random.Next();
-
-               Console.WriteLine("====>" + randomIndex);
-                Console.WriteLine("--------------------------------------");
-
-
-                Console.WriteLine("Name: " + test[randomIndex].Name);
-
-                Console.WriteLine("Price:" + test[randomIndex].Price);
-                Console.WriteLine("===>" + test[randomIndex].Recipe.Length);
-
-                /*
-                for (int i = 0; i > 0; i++)
+                Console.WriteLine($"Recipe label: {recipe.label}"); //can put whatever info from meal.cs (gotten from API)
+                Console.WriteLine($"Country: {country}");
+                Console.WriteLine($"Recipe ingredients:");
+                foreach (var ingredient in recipe.ingredientLines)//using foreach since the ingredients are in a list
                 {
-                    
-                }*/
-
-
-
-
-                //Meal randomMeal = meals[random.Next(0, meals.Length)];
-                if (selectedMeals.Any())
-                {
-                    foreach (var meal in selectedMeals)
-                    {
-                        Console.WriteLine($"Name: {meal.Name}");
-                        Console.WriteLine($"Price: {meal.Price}");
-                        if (meal.Recipe != null)
-                        {
-                            //Console.WriteLine("Recipe:");
-                            //foreach (var recipe in selectedMeal.Recipe)
-                            //{
-                            //    Console.WriteLine(recipe);
-                            //}
-
-                            Console.WriteLine($"Recipe: {string.Join(", ", meal.Recipe)}");
-                            //foreach (var recipe in randomMeal.Recipe)
-                            //{
-                            //   Console.WriteLine(recipe);
-                            //}
-                        }
-                        Console.WriteLine("");
-                    }
+                    Console.WriteLine($"  {ingredient}");
                 }
-                else
-                {
-                    Console.WriteLine($"Meal with country '{input}' not found!");
-                }
-                Console.ReadLine();
+                Console.WriteLine($"Total calories: {recipe.calories}");
+                Console.WriteLine($"Source: {recipe.source} | {recipe.url}");
+            }
+            else //gives if recipe count is 0
+            {
+                Console.WriteLine("sorry, no recipes found!");
             }
         }
+
+        //Example 1
+        //static void GetRandomRecepe(Meal[] meals)
+        //{
+        //    if (meals != null)
+        //    {
+        //        Console.Write("Enter country: ");
+        //        string input = Console.ReadLine();
+
+        //        // We are filtering by the country. 
+        //        var selectedMeals = meals.Where(m => m.Country.ToLower() == input.ToLower());
+
+        //        // Checking amount. If  0, will be else. 
+        //        if (selectedMeals.Count() > 0)
+        //        {
+        //            // If we have more than 1 we will take random one of them else if 1 we will take the first one. 
+        //            Meal selectedMeal = selectedMeals.Count() > 1 ? selectedMeals.ElementAt(new Random().Next(0, selectedMeals.Count())) : selectedMeals.First();
+
+        //            // Showing the results.
+        //            Console.WriteLine("Random meal selected: {0}", selectedMeal.Name);
+        //            Console.WriteLine("Price: {0}", selectedMeal.Price);
+
+        //            // Do we have recipe data.
+        //            if (selectedMeal.Recipe != null)
+        //            {
+        //                Console.WriteLine($"Recipe: {string.Join(", ", selectedMeal.Recipe)}");
+        //            }
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("No meals found with that country.");
+        //        }
+        //        //    Console.ReadLine();
+        //    }
+        //}
     }
 }
